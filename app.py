f67176104b63a34b3a0b8ad35f79cb2ef970db2a
Mraimundo <mdumundo18@gmail.com>
2026-04-26 18:26:32 -0300

    feat: :sparkles: implement task management API with CRUD operations and add Task model

--- app.py
+++ app.py
@@ -1,14 +1,73 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "Hello World!"
+tasks = []
+task_id_control = 1
+@app.route("/tasks", methods=["POST"])
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(
+    id=task_id_control,
+    title=data["title"],
+    description=data.get("description")
+  )
+  task_id_control += 1
 
-@app.route("/greet/<name>")
-def greet(name):
-  return f"Hello, {name}!"
+  tasks.append(new_task)
+
+  return jsonify({"message": "Nova tarefa criada com sucesso!"})
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+  task_list= [task.to_dict() for task in tasks]
+  
+  output = {"tasks": task_list, "total_tasks": len(task_list)}
+  
+  return jsonify(output)
+
+@app.route("/tasks/<int:task_id>", methods=["GET"])
+def get_task(task_id):
+  for t in tasks:
+    if t.id == task_id:
+      return jsonify(t.to_dict())
+  return jsonify({"message": "Tarefa não encontrada"}), 404     
+
+@app.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+  task = None
+  for t in tasks:
+    if t.id == task_id:
+      task = t
+      break
+
+  if task == None:
+      return  jsonify({"message": "Tarefa não encontrada"}), 404    
+
+  data = request.get_json()
+  task.title = data["title"]
+  task.description = data["description"]
+  task.completed = data["completed"]
+  return jsonify({"message": "Tarefa atualizada com sucesso!"})
+
+
+@app.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+  task = None
+  for t in tasks:
+    if t.id == task_id:
+      task = t
+      break
+
+  if not task:
+      return  jsonify({"message": "Tarefa não encontrada"}), 404    
+
+  tasks.remove(task)
+  return jsonify({"message": "Tarefa excluída com sucesso!"})
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
